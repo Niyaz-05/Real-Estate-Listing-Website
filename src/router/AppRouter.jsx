3b5ab74612a1e619ff8a1bcfc58d0929495d1274
Niyaz Khan <savagebaba00@gmail.com>
2025-11-29 12:04:06 +0530

    Added full app routing with navbar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,30 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home/Home.jsx";
-import Listings from "../pages/Listings/listings.jsx";
-import PropertyDetails from "../pages/PropertyDetails/PropertyDetails.jsx";
-import Login from "../pages/Login/Login.jsx";
-import AdminDashboard from "../pages/Admin/AdminDashboard.jsx";
-import AddProperty from "../pages/AddProperty/AddProperty.jsx";
-import Navbar from "../components/Navbar/Navbar.jsx";
-import Footer from "../components/Footer/Footer.jsx";
+import { Home } from "../Pages/Home/Home";
+import { Listings } from "../Pages/Listings/Listings";
+import { AddProperty } from "../Pages/AddProperty/AddProperty";
+import { Login } from "../Pages/Login/Login";
+import { PropertyDetails } from "../Pages/PropertyDetails/PropertyDetails";
+import { Navbar } from "../components/Navbar/Navbar";
 
-const AppRouter = () => {
+export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/listings" element={<Listings />} />
         <Route path="/property/:id" element={<PropertyDetails />} />
+        <Route path="/add-property" element={<AddProperty />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/admin/add-property" element={<AddProperty />} />
       </Routes>
-
-      <Footer />
     </BrowserRouter>
   );
 };
-
-export default AppRouter;
